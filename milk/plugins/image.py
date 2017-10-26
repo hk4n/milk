d92d6e5a612799b0fd09e3f2f6659edddb06ad92
hk4n <hakan@focotech.se>
2017-10-26 15:27:47 +0200

    added logging and done some cleanup to the image module

--- milk/plugins/image.py
+++ milk/plugins/image.py
@@ -2,6 +2,7 @@ from milk.plugin import Plugin
 
 import docker
 import os
+import logging
 
 
 class image(Plugin):
@@ -12,39 +13,41 @@ class image(Plugin):
         if self.client is None:
             self.client = docker.from_env(version='auto')
 
-        print(self.client)
-
         if "build" in config:
             build(self.client, config["build"])
 
         elif "remove" in config:
-            remove(self.client, config["remove"])
+            remove(self.client, config)
 
 
 class build():
     def __init__(self, client, config):
-        print(client)
-        print(config)
         if "dockerfile" not in config:
             raise Exception("'dockerfile' is missing")
-        else:
-            if not os.path.isabs(config["dockerfile"]):
-                config["dockerfile"] = os.path.join(os.getcwd(), config["dockerfile"])
-
-            if not os.path.isfile(config["dockerfile"]):
-                raise Exception("'dockerfile' does not contain the correct path: %s " % config["dockerfile"])
 
         if "tag" not in config:
             raise Exception("'tag' is missing")
 
+        if not os.path.isabs(config["dockerfile"]):
+            config["dockerfile"] = os.path.join(os.getcwd(), config["dockerfile"])
+
+        if not os.path.isfile(config["dockerfile"]):
+            raise Exception("'dockerfile' does not contain the correct path: %s " % config["dockerfile"])
+
         try:
-            print(client.images.build(**config))
-        except docker.errors.BuildError as e:
-            print(e)
+            image = client.images.build(**config)
+            logging.info("Created image '%s' id: %s" % (config["tag"], image.attrs["Id"]))
+        except docker.errors.BuildError:
             raise
 
 
 class remove():
     def __init__(self, client, config):
-        print("Removed: %s" % client.images.remove(image=config))
-        # implement prune and force
+        image = config["remove"]
+        config.pop("remove")
+        try:
+            client.images.remove(image=image, **config)
+        except docker.errors.ImageNotFound:
+            raise
+
+        logging.info("Removed: %s" % image)
